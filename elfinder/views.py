834bf5d0599fbcd0f30f3e284d32ce39d1e6782d
alexsilva <geniofuturo@gmail.com>
2023-08-09 17:21:02 -0300

    Views prepared to work with multiple volumes.

--- elfinder/views.py
+++ elfinder/views.py
@@ -4,117 +4,106 @@ from django.contrib.auth.decorators import user_passes_test
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.utils.functional import cached_property
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import ensure_csrf_cookie
-
+from django.views.generic import View
 from elfinder.conf import settings
 from elfinder.connector import ElFinderConnector
 from elfinder.volume_drivers import get_volume_driver
 
 
-class VolumeDriver(object):
-    def __init__(self, request, json_response=False, name=None, **options):
-        self.request = request
-        self.options = options
-        self.json_response = json_response
-        self._name = name
+class VolumeMixin:
+    json_response = False
 
-    @cached_property
-    def name(self):
-        if self._name is None:
-            request_method = getattr(self.request, self.request.method)
-            return request_method.get('volume', 'default')
+    def dispatch(self, request, *args, **kwargs):
+        self.volumes = self.get_volume_drivers(request, collection_id=kwargs.get('coll_id'))
+        for volume_driver in self.volumes:
+            if volume_driver.login_required and (login_view := self.get_login_view(request, volume_driver)):
+                return login_view
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_volume_drivers(self, request, **options) -> list:
+        """Returns a list with volumes"""
+        volume_drivers = []
+        options.setdefault('request', request)
+        for volume_name in self.get_volume_names(request):
+            volume = get_volume_driver(volume_name, **options)
+            volume_drivers.append(volume)
+        return volume_drivers
+
+    def get_volume_names(self, request):
+        return request.GET.getlist('volume', ['default'])
+
+    def login_test(self, request):
+        """Executing this method indicates that login validation has passed and no authentication is required."""
+        return False
+
+    def get_login_view(self, request, volume):
+        """Checks if volume is project by authentication (redirect to view accordingly)."""
+        decorator = user_passes_test(test_func=volume.login_test_func,
+                                     login_url=volume.login_url)
+        response = decorator(self.login_test)(request)
+        if response:
+            if self.json_response:
+                return JsonResponse({'error': "Login required!"})
+            else:
+                return response
+        return response
+
+
+class IndexView(VolumeMixin, View):
+
+    @method_decorator(ensure_csrf_cookie)
+    @method_decorator(never_cache)
+    def get(self, request, coll_id=None):
+        """ Displays the elFinder file browser template for the specified collection.
+        """
+        volume = self.volumes[0]
+        context = {
+            'coll_id': coll_id,
+            'volume_driver': volume
+        }
+        return render(request,
+                      # Possibility to configure a custom template
+                      volume.get_index_template("elfinder/index.html"),
+                      context=context,
+                      using=settings.ELFINDER_TEMPLATE_ENGINE)
+
+
+class ConnectorView(VolumeMixin, View):
+    http_method_names = ['get', 'post']
+    json_response = True
+
+    def post(self, request, **kwargs):
+        return self.get(request, **kwargs)
+
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request, coll_id=None):
+        """ Handles requests for the elFinder connector.
+        """
+        finder = ElFinderConnector(self.volumes)
+        try:
+            finder.run(request)
+        except:
+            if settings.ELFINDER_DEBUG:
+                import traceback
+                traceback.print_exc()
+            raise
+        # Some commands (e.g. read file) will return a Django View - if it
+        # is set, return it directly instead of building a response
+        if finder.return_view:
+            return finder.return_view
+
+        response = HttpResponse(content_type=finder.httpHeader['Content-type'])
+        response.status_code = finder.httpStatusCode
+        if finder.httpHeader['Content-type'] == 'application/json':
+            response.content = json.dumps(finder.httpResponse,
+                                          cls=DjangoJSONEncoder,
+                                          ensure_ascii=False)
         else:
-            return self._name
-
-    @cached_property
-    def volume(self):
-        volume = get_volume_driver(self.name,
-                                   request=self.request,
-                                   **self.options)
-        return volume
-
-    @staticmethod
-    def _access_view(req):
-        return True
-
-    def __bool__(self):
-        return self.login_view is True
-
-    __nonzero__ = __bool__
-
-    @cached_property
-    def login_view(self):
-        """Checks if volume is project by authentication.
-        (redirect to view accordingly)"""
-        if self.volume.login_required:
-            decorator = user_passes_test(test_func=self.volume.login_test_func,
-                                         login_url=self.volume.login_url)
-            response = decorator(self._access_view)(self.request)
-            if response is not True:
-                if self.json_response:
-                    return JsonResponse({'error': "Login required!"})
-                else:
-                    return response
-        return True
-
-
-@ensure_csrf_cookie
-@never_cache
-def index(request, coll_id=None):
-    """ Displays the elFinder file browser template for the specified
-        collection.
-    """
-    volume_driver = VolumeDriver(request, collection_id=coll_id)
-
-    if not volume_driver:  # not has access
-        return volume_driver.login_view
-    context = {
-        'coll_id': coll_id,
-        'volume_driver': volume_driver
-    }
-    return render(request,
-                  # Possibility to configure a custom template
-                  volume_driver.volume.get_index_template("elfinder/index.html"),
-                  context=context,
-                  using=settings.ELFINDER_TEMPLATE_ENGINE)
-
-
-@ensure_csrf_cookie
-def connector_view(request, coll_id=None):
-    """ Handles requests for the elFinder connector.
-    """
-    volume_driver = VolumeDriver(request,
-                                 json_response=True,
-                                 collection_id=coll_id)
-
-    if not volume_driver:  # not has access
-        return volume_driver.login_view
-
-    finder = ElFinderConnector([volume_driver.volume])
-    try:
-        finder.run(request)
-    except:
-        if settings.ELFINDER_DEBUG:
-            import traceback
-            traceback.print_exc()
-        raise
-    # Some commands (e.g. read file) will return a Django View - if it
-    # is set, return it directly instead of building a response
-    if finder.return_view:
-        return finder.return_view
-
-    response = HttpResponse(content_type=finder.httpHeader['Content-type'])
-    response.status_code = finder.httpStatusCode
-    if finder.httpHeader['Content-type'] == 'application/json':
-        response.content = json.dumps(finder.httpResponse,
-                                      cls=DjangoJSONEncoder,
-                                      ensure_ascii=False)
-    else:
-        response.content = finder.httpResponse
-
-    return response
+            response.content = finder.httpResponse
+        return response
 
 
 def read_file(request, volume, file_hash, template="elfinder/read_file.html"):
@@ -126,3 +115,7 @@ def read_file(request, volume, file_hash, template="elfinder/read_file.html"):
     return render(request, template,
                   context={'file': file_hash},
                   using=settings.ELFINDER_TEMPLATE_ENGINE)
+
+
+index = IndexView.as_view()
+connector_view = ConnectorView.as_view()
