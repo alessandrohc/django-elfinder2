b6a0cbbf26af8bbd79452b1241259c2ba7e60f6d
alexsilva <geniofuturo@gmail.com>
2024-08-21 17:32:58 -0300

    Update revision to 64.

--- elfinder/connector.py
+++ elfinder/connector.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 class ElFinderConnector(object):
     _api_version = 2.1
-    _api_revision = 61
+    _api_revision = 64
 
     def __init__(self, volumes=None):
         if volumes is None:
